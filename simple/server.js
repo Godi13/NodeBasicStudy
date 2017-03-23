f5bebc657b3a3c8682e3551698f8dfc99a3edb44
godi13 <mqzq9388@gmail.com>
2017-03-23 15:39:54 +0800

    chat-unfinished

--- simple/server.js
+++ simple/server.js
@@ -7,6 +7,7 @@ var server = http.createServer()
 // }).listen(3000)
 
 server.on('request', function(req, res) {
+  console.log(req);
   res.writeHead(200, {'Content-Type': 'text/plain'})
   res.end('Hello World\n')
 })
